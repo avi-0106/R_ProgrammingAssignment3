1b4dc4b6fff1e57a1e2a641ea4c412bcb154cab8
avi-0106 <abhishekmishra0106@gmail.com>
2024-01-20 16:27:52 +0530

    Updated rankhospital.R[2]

--- rankhospital.R
+++ rankhospital.R
@@ -1,4 +1,5 @@
 library(stringr)
+library(dplyr)
 
 rankhospital <- function(state, outcome, num = "best"){
   
@@ -25,6 +26,8 @@ rankhospital <- function(state, outcome, num = "best"){
   
   state_select <- subset(outcome_data, outcome_data$State == state)
   state_select <- state_select[!is.na(state_select[, outcome_column]), ]
+  state_select[, outcome_column] <- trimws(state_select[, outcome_column])
+  state_select <- state_select[state_select[, outcome_column] != "Not Available", ]
   
   rank_count <- nrow(state_select)
   if (is.numeric(num)) {
@@ -32,7 +35,10 @@ rankhospital <- function(state, outcome, num = "best"){
       return(NA)
     }
     else{
-      #dddddddddddddddddddddddddddd
+      #state_select <- state_select[order(as.numeric(state_select[,outcome_column]), state_select$Hospital.Name)]
+      ordered_state <- state_select %>% arrange(as.numeric(!!sym(outcome_column)), Hospital.Name)
+      ranked_hospital <- state_select$Hospital.Name[num]
+      return(ranked_hospital)
     }
   }
   else if (num == "best"){
